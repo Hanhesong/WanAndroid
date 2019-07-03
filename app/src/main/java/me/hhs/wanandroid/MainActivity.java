ca1960eaa246e188f63828d2af5ba3fad63309a6
Hanhesong <hesong.han@gmail.com>
2019-07-03 16:39:53 +0800

    use Banner but current project has a bug that the image can't show

--- app/src/main/java/me/hhs/wanandroid/MainActivity.java
+++ app/src/main/java/me/hhs/wanandroid/MainActivity.java
@@ -1,13 +1,61 @@
 package me.hhs.wanandroid;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.bumptech.glide.Glide;
+import com.ms.banner.Banner;
+import com.ms.banner.holder.BannerViewHolder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.banner)
+    Banner banner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        String[] imagePath = getResources().getStringArray(R.array.imagePath);
+        List list = new ArrayList(Arrays.asList(imagePath));
+       // banner.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT/4));
+        banner.setAutoPlay(true)
+                .setPages(list, new CustomViewHolder())
+                .setDelayTime(3000)
+                .start();
+    }
+
+    public class CustomViewHolder implements BannerViewHolder<Object> {
+        private ImageView mImageView;
+
+        @Override
+        public View createView(Context context) {
+            mImageView = new ImageView(context);
+            ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+            mImageView.setLayoutParams(layoutParams);
+            mImageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            return mImageView;
+        }
+
+        @Override
+        public void onBind(Context context, int position, Object data) {
+            Glide.with(context).load(data).into(mImageView);
+        }
     }
 }
